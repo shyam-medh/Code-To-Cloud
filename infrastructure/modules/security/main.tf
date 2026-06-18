92ac5d582b04e8f4da6461d035ff142dce401c16
shyam-medh <baranwal07shyam@gmail.com>
2026-06-19 00:16:49 +0530

    Add Port 9000 for SonarQube access on Jenkins Slave

--- infrastructure/modules/security/main.tf
+++ infrastructure/modules/security/main.tf
@@ -183,6 +183,14 @@ resource "aws_security_group" "jenkins_slave" {
     security_groups = [aws_security_group.jenkins_master.id]
   }
 
+  ingress {
+    description = "SonarQube UI"
+    from_port   = 9000
+    to_port     = 9000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
